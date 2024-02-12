4fe57f4aaedf86b4ac35118ed02e3d6fbd5ac640
vova836 <123488703+vova836@users.noreply.github.com>
2024-02-12 15:40:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Themes18;
+
+public static class Program
+{
+	public static void Main()
+	{
+    MyClass obj1 = new MyClass(3.0, 4.0);
+    MyClass obj2 = new MyClass();
+    MyClass obj3 = new MyClass(5.0, 6.0);
+
+Console.WriteLine($"Количество созданных экземпляров:: {MyClass.count}");
+
+obj1.PrintValues();
+obj2.PrintValues();
+obj3.PrintValues();
+
+	}
+}
